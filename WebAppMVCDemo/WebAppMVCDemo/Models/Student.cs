4b64a2c2904102f13b16baafba6fbdee93b91535
lxxsgy <1197910505@qq.com>
2018-11-06 10:50:19 +0800

    添加webMVCdemo

---
+++ WebAppMVCDemo/WebAppMVCDemo/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVCDemo.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Gender { get; set; }
+        public string Major { get; set; }
+        public DateTime EntranceDate { get; set; }
+
+    }
+}
---
+++ myFirstWebApplication/myFirstWebApplication/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myFirstWebApplication.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Gender { get; set; }
+        public string Major { get; set; }
+        public DateTime EntranceDate { get; set; }
+    }
+}
