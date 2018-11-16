33c1fb77f199e6f35c8137f65b6ceefcff898148
lxxsgy <1197910505@qq.com>
2018-11-16 13:36:49 +0800

    添加opencv和leedtools结合使用OCR

---
+++ LeedtoolsModules/LeedtoolsModules/MainForm.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace LeedtoolsModules
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImageDragOCRPage = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.rasterImageViewer1 = new Leadtools.WinForms.RasterImageViewer();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.checkBoxLanguage = new System.Windows.Forms.CheckBox();
+            this.ImageDragOCRPage.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ImageDragOCRPage
+            // 
+            this.ImageDragOCRPage.Controls.Add(this.panel1);
+            this.ImageDragOCRPage.Location = new System.Drawing.Point(4, 22);
+            this.ImageDragOCRPage.Name = "ImageDragOCRPage";
+            this.ImageDragOCRPage.Padding = new System.Windows.Forms.Padding(3);
+            this.ImageDragOCRPage.Size = new System.Drawing.Size(780, 450);
+            this.ImageDragOCRPage.TabIndex = 0;
+            this.ImageDragOCRPage.Text = "图像识别";
+            this.ImageDragOCRPage.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(774, 444);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.67568F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.32433F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(774, 444);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkBoxLanguage);
+            this.panel2.Controls.Add(this.button12);
+            this.panel2.Controls.Add(this.button11);
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Controls.Add(this.richTextBox4);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(766, 107);
+            this.panel2.TabIndex = 0;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(222, 65);
+            this.button12.Margin = new System.Windows.Forms.Padding(2);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(56, 23);
+            this.button12.TabIndex = 26;
+            this.button12.Text = "缩小";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(143, 65);
+            this.button11.Margin = new System.Windows.Forms.Padding(2);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(56, 23);
+            this.button11.TabIndex = 25;
+            this.button11.Text = "放大";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(312, 65);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(56, 23);
+            this.button8.TabIndex = 24;
+            this.button8.Text = "标记";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(456, 18);
+            this.richTextBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(290, 87);
+            this.richTextBox4.TabIndex = 23;
+            this.richTextBox4.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(39, 18);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 12);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "选择要识别的图像：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(41, 32);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(292, 21);
+            this.textBox4.TabIndex = 21;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(368, 32);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(56, 18);
+            this.button7.TabIndex = 20;
+            this.button7.Text = ">>>";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.rasterImageViewer1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(4, 118);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(766, 322);
+            this.panel3.TabIndex = 1;
+            // 
+            // rasterImageViewer1
+            // 
+            this.rasterImageViewer1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.rasterImageViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rasterImageViewer1.Location = new System.Drawing.Point(0, 0);
+            this.rasterImageViewer1.Name = "rasterImageViewer1";
+            this.rasterImageViewer1.Size = new System.Drawing.Size(766, 322);
+            this.rasterImageViewer1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.ImageDragOCRPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(788, 476);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // checkBoxLanguage
+            // 
+            this.checkBoxLanguage.AutoSize = true;
+            this.checkBoxLanguage.Location = new System.Drawing.Point(81, 71);
+            this.checkBoxLanguage.Name = "checkBoxLanguage";
+            this.checkBoxLanguage.Size = new System.Drawing.Size(48, 16);
+            this.checkBoxLanguage.TabIndex = 27;
+            this.checkBoxLanguage.Text = "中文";
+            this.checkBoxLanguage.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 476);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ImageDragOCRPage.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabPage ImageDragOCRPage;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Panel panel3;
+        private Leadtools.WinForms.RasterImageViewer rasterImageViewer1;
+        private System.Windows.Forms.CheckBox checkBoxLanguage;
+    }
+}
+
---
+++ LeedtoolsModules/LeedtoolsModules/MainForm.cs
@@ -0,0 +1,196 @@
+﻿using Emgu.CV;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+using Leadtools;
+using Leadtools.Annotations;
+using Leadtools.Codecs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeedtoolsModules
+{
+    public partial class MainForm : Form
+    {
+        Leadtools.Codecs.RasterCodecs m_codecs = null;
+        // private IOcrDocument _ocrDocument;
+        //private IOcrEngine ocrEngine = null;
+        private AnnAutomation annAutomation = null;
+        private AnnAutomationManager annAutoManager = null;
+        public MainForm()
+        {
+            InitializeComponent();
+            Leadtools.Codecs.RasterCodecs.Startup();
+            m_codecs = new RasterCodecs();
+            //ocrEngine = OcrEngineManager.CreateEngine(OcrEngineType.Professional, false);
+            //ocrEngine.Startup(null, null, null, @"C:\Program Files (x86)\LEAD Technologies\LEADTOOLS 16.5\Bin\Common\OcrProfessionalRuntime");
+            annAutoManager = new AnnAutomationManager();
+            //创建默认的自动化对象
+            annAutoManager.CreateDefaultObjects();
+            AnnAutomationObject hiliteObject = annAutoManager.FindObject(AnnAutomationManager.HiliteObjectId);
+            ContextMenu hiliteObjectMenu = hiliteObject.ContextMenu;
+            foreach (MenuItem menuItem in hiliteObjectMenu.MenuItems)
+            {
+                //if (menuItem.Text.ToUpper() != "&DELETE")
+                {
+                    menuItem.Visible = true;
+                }
+            }
+            annAutomation = new AnnAutomation(annAutoManager, rasterImageViewer1);
+            annAutomation.Active = true;
+            annAutoManager.UserMode = AnnUserMode.Design;
+            annAutomation.Container.Objects.ItemAdded += new EventHandler<RasterCollectionEventArgs<AnnObject>>(Objects_ItemAdded);
+            annAutomation.Container.Objects.ItemRemoved += new EventHandler<RasterCollectionEventArgs<AnnObject>>(Objects_ItemRemoved);
+            annAutomation.AfterObjectChanged += new EventHandler<AnnAfterObjectChangedEventArgs>(annAuto_AfterObjectChanged);
+
+            //当AnnAutomationManager对象的UserMode被设置为Design时去掉所有自动化对象的选择手柄
+            foreach (AnnAutomationObject obj in annAutoManager.Objects)
+            {
+                obj.UseRotateControlPoints = false;
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Objects_ItemRemoved(object sender, RasterCollectionEventArgs<AnnObject> e)
+        {
+
+
+        }
+
+        private void Objects_ItemAdded(object sender, RasterCollectionEventArgs<AnnObject> e)
+        {
+
+            annAutomation.Container.Objects.Clear();
+        }
+        private void annAuto_AfterObjectChanged(object sender, AnnAfterObjectChangedEventArgs e)
+        {
+
+            switch (e.ChangeType)
+            {
+                case AnnObjectChangedType.DesignerDraw:
+                    if (e.Objects.Count > 0)
+                    {
+                        AnnObject ann = e.Objects[0];
+                        AnnRectangle annRect = ann.ConvertObjectToLogicalRectangle(ann.BoundingRectangle);
+                        Int32 width, height, top, left;
+                        height = (Int32)annRect.Height;
+                        width = (Int32)annRect.Width;
+                        top = (Int32)annRect.Top;
+                        left = (Int32)annRect.Left;
+                        if (top > 0 && height > 0 && top > 0 && left > 0)
+
+
+                        {
+                            Rectangle rectangle = new Rectangle(left, top, width, height);
+                            using (RasterImage image = this.rasterImageViewer1.Image.Clone(rectangle))
+                            {
+
+                                using (MemoryStream stream = new MemoryStream())
+                                {
+                                    this.m_codecs.Save(image, stream, image.OriginalFormat, image.BitsPerPixel);
+                                   
+                                    Bitmap Bitmap = new Bitmap(stream);
+
+                                    string temp = GetOCRText(Bitmap,checkBoxLanguage.Checked? "中文":"");
+                                    //byte[] bytes = File.ReadAllBytes("123.tif");
+                                    //OCRServiceClient clinet = new OCRServiceClient
+                                    //    ();
+                                    //var ocrresult = clinet.OCRCutSnippet(bytes);
+                                    //string temp = ocrresult.OCRResult;
+                                    //clinet.Close();
+
+                                    richTextBox4.Text = "识别结果：" + temp;
+
+                                }
+                            }
+                        }
+
+
+                    }
+                    break;
+            }
+        }
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox4.Text = ofd.FileName;
+            }
+            rasterImageViewer1.Image = m_codecs.Load(textBox4.Text);
+            rasterImageViewer1.Show();
+            //int count = rasterImageViewer1.Image.PageCount;
+            //for (int i = 0; i < count; i++)
+            //{
+            //    RasterCodecs m_code = new RasterCodecs();
+            //    string newImage = i.ToString().PadLeft(8, '0') + ".tiff";
+
+            //    using (RasterImage rimg = m_codecs.Load(textBox4.Text.Trim(), 0, CodecsLoadByteOrder.Bgr, i + 1, i + 1))
+            //    {
+            //        m_codecs.Save(rimg, newImage, RasterImageFormat.Tif, rimg.BitsPerPixel, 1, 1, 1, CodecsSavePageMode.Overwrite);
+            //    }
+
+            //}
+            annAutoManager.CurrentObjectId = AnnAutomationManager.HiliteObjectId;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            annAutoManager.CurrentObjectId = AnnAutomationManager.HiliteObjectId;
+        }
+
+        private string GetOCRText(Bitmap bit,string languageType)
+        {
+            string language = "eng";//识别语言
+            if (languageType=="中文")
+            {
+                language = "chi_sim";
+            }
+           
+            string value = "";
+            string path = Application.StartupPath + "\\tessdata";  //下载识别文件夹
+            Tesseract ocr = new Tesseract(path, language, Emgu.CV.OCR.OcrEngineMode.Default);//生成OCR对象。
+            Emgu.CV.Image<Gray, byte> GAY1 = new Image<Gray, byte>(bit);//原图一定为灰阶图片
+            ocr.SetImage(GAY1);//放进图像到OCR对象中
+            int A_LENG = 2;
+            A_LENG = ocr.Recognize();//进行识别图像
+            if (A_LENG == 0)  //判断是否成功，应为返回为0时才是成功，因此开始定义初始化为其他值（非零）。
+            {
+                // MessageBox.Show("ok");
+                Tesseract.Character[] tet = ocr.GetCharacters();//得到
+                 value = ocr.GetUTF8Text();//得到文字
+            }
+            else
+            {
+                value = "fail to ocr";
+            }
+           
+            return value;
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            rasterImageViewer1.ScaleFactor += 0.5;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            rasterImageViewer1.ScaleFactor -= 0.5;
+        }
+    }
+}
