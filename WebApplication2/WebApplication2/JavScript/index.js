4865854117adaea310647a93a6a49e6e02f8931c
lxxsgy <1197910505@qq.com>
2019-01-07 10:18:30 +0800

    添加功能

--- WebApplication2/WebApplication2/JavScript/index.js
+++ WebApplication2/WebApplication2/JavScript/index.js
@@ -1,8 +1,28 @@
-﻿ $(function () {
+﻿$(function () {
+    $.extend($.fn.datagrid.defaults.editors, {
+        text: {
+            init: function (container, options) {
+                var input = $('<input type="text" class="datagrid-editable-input">').appendTo(container);
+                return input;
+            },
+            destroy: function (target) {
+                $(target).remove();
+            },
+            getValue: function (target) {
+                return $(target).val();
+            },
+            setValue: function (target, value) {
+                $(target).val(value);
+            },
+            resize: function (target, width) {
+                $(target)._outerWidth(width);
+            }
+        }
+    });
+    var lastRowIndex=0;
      $('#dg').datagrid({
 
          url: '../handler/MainDemoHandler.ashx',
-         //url:'dataGrid.json',
          method: 'post',
          rownumbers: true,
          checkOnSelect: false,
@@ -22,13 +42,14 @@
                  title: '包号',
                  sortable: true,
                  width: 100,
+                 align: 'center',
+                 halign: 'center',
+                 order: 'asc',
                  editor: {
                      type: 'text',
                      options: {
 
-                         required:true
                      }
-
                  }
              },
              {
@@ -36,46 +57,106 @@
                 title: '包名',
                  sortable: true,
                  width: 100,
+                 align: 'center',
+                 halign: 'center',
+                 order: 'asc',
                  editor: {
                      type: 'text',
                      options: {
 
-                         required: true
                      }
-
                  }
-               
+                
             }, {
                 field: 'ProjectId',
                 title: '项目ID',
                  sortable: true,
                  width: 100,
-                editor: {
+                 align: 'center',
+                 halign: 'center',
+                 order: 'asc',
+                 //checkbox:true
+                 editor: {
                      type: 'text',
                      options: {
 
-                         required: true
                      }
-
                  }
-             },
-         ]] 
+             }
+             
+         ]],
+         //onClickRow: function (rowIndex,rowData) {
+
+            
+         //        $("#dg").datagrid("endEdit", lastRowIndex);
+         //        $("#dg").datagrid("beginEdit", rowIndex);
+         //      lastRowIndex = rowIndex;
+             
+         //},
+         //onClickCell: function (rowIndex, field, value) {
+
+            
+         //},
+         onAfterEdit: function (rowIndex, rowIndex, changes) {
+
+            
+
+
+
+         },
+         //onCancelEdit: function (rowIndex, rowIndex) {
+
+            
+         //},
+         //onSelect: function (rowIndex, rowdata) {
+         //   // alert(rowIndex);
+         //},
+         //onUnselect: function (rowIndex,rowdata) {
+         //    alert(rowIndex);
+         //},
+         //onCheck: function (rowIndex, rowData) {
+         //    console.log(rowData);
+         //},
+         //onSelectAll: function (rows) {
+           
+         //}
+        
         
      });
      
      
 
     
-     obj = {
-         search: function () {
-             $("#dg").datagrid('load', {
-                 packageId: $.trim($('input[name="packageid"]').val()),
-                 pname: $('input[name="pname"]').val(),
-                 projectId: $('input[name="projectid"]').val(),
-             }
-             );
-         }
-     };
+     //obj = {
+     //    search: function () {
+     //      //  console.log($("#dg").datagrid('getSelections'));
+     //        $("#dg").datagrid('checkAll');
+     //       // console.log($("#dg").datagrid('getRows'));
+     //    }
+     //};
+    obj = {
+        search: function () {
+            $("#dg").datagrid('load', {
+                packageId: $.trim($('input[name="packageid"]').val()),
+                pname: $('input[name="pname"]').val(),
+                projectId: $('input[name="projectid"]').val(),
+            }
+            );
+        },
+        add: function () {
+
+        },
+        edit: function () {
+
+        },
+        remove: function () {
+
+        }
+    };
+
+
+
+
    
                     
 
